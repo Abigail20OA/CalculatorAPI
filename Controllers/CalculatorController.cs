738744ea8e934f248be226851c15d0adbb28d086
Abigail Osinaga <a2024110495@estudiantes.upsa.bo>
2025-08-20 20:26:03 -0400

    DIVISION Y MULTIPLICACION

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -2,16 +2,37 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CalculatorAPI.Controllers
 {
-[Route(template:"api/[controller]")]
-[ApiController]
-public class CalculatorController : ControllerBase
-{
-    [HttpGet("sum")]
-    public IActionResult GetSum([FromQuery] int a, [FromQuery] int b)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
     {
-        var result = a + b;
-        return Ok(new { a, b, result });
+        [HttpGet("sum")]
+        public IActionResult GetSum([FromQuery] int a, [FromQuery] int b)
+        {
+            var result = a + b;
+            return Ok(new { a, b, result });
+        }
+
+        [HttpGet("sub")]
+        public IActionResult GetSub([FromQuery] int a, [FromQuery] int b)
+        {
+            var result = a - b;
+            return Ok(new { a, b, result });
+        }
+
+        [HttpGet("mul")]
+        public IActionResult GetMul([FromQuery] int a, [FromQuery] int b)
+        {
+            var result = a * b;
+            return Ok(new { a, b, result });
+        }
+
+        [HttpGet("div")]
+        public IActionResult GetDiv([FromQuery] int a, [FromQuery] int b)
+        {
+            if (b == 0) return BadRequest(new { message = "No se puede dividir entre 0" });
+            double result = (double)a / b;
+            return Ok(new { a, b, result });
+        }
     }
-}
-   
 }
